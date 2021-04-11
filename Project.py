ede17f1337f9dd75950a6bdedf84be2f900c3a8d
LemonU <lassaegsi@gmail.com>
2021-04-11 12:10:34 -0400

    added __str__ method

--- Project.py
+++ Project.py
@@ -20,3 +20,15 @@ class Project:
 
     def get_admin(self) -> User:
         return self.admin
+
+    def __str__(self):
+        return '''uid:\t%d
+        name:\t%s
+        date_created:\t%s
+        due_date:\t%s
+        member:\t%s
+        admin:\t%s
+        completed:\t%s
+        description:\t%s''' % (self.uid, self.name, self.date_created, \
+            self.due_date, [m.get_login_name() for m in self.member], \
+                self.admin.get_login_name(), self.completed, self.description)
