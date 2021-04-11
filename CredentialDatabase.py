8e42433a3de6818046d2aaad475c2e7156fa3aa6
LemonU <lassaegsi@gmail.com>
2021-04-11 13:04:26 -0400

    added skeleton code for databases

--- CredentialDatabase.py
+++ CredentialDatabase.py
@@ -2,8 +2,14 @@ from User import User
 
 class CredentialDatabase:
 
+    __json_dump_file_name = "core_user_record.json"
     credentials_login_name_to_user = {}
     credentials_uid_to_user = {}
+
+    __instance = None
+
+    def __init__(self):
+        pass
 
     def get_user_from_id(self, id: int) -> User:
         pass
@@ -13,3 +19,12 @@ class CredentialDatabase:
 
     def add_user(self, user: User):
         pass
+
+    @staticmethod
+    def get_instance() -> CredentialDatabase:
+        if CredentialDatabase.__instance == None:
+            CredentialDatabase.__instance = CredentialDatabase()
+        return CredentialDatabase.__instance
+
+if __name__ == "__main__":
+    pass
---
+++ ProjectDataBase.py
@@ -0,0 +1,23 @@
+from User import User
+from Project import Project
+
+class ProjectDataBase:
+
+    def add_project(self, project: Project):
+        pass
+
+    def change_project(self, project_id: int, project: Project):
+        pass
+
+    def delete_project(self, project_id: int):
+        pass
+
+    def get_project(self, project_id: int):
+        pass
+
+    def get_user_projects(self, user: User) -> list:
+        pass
+
+    @staticmethod
+    def get_instance() -> ProjectDataBase:
+        pass
