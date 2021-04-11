936efeff0038d7dece679ce0906e70f421e0bfc7
LemonU <lassaegsi@gmail.com>
2021-04-10 23:57:51 -0400

    skeleton for this class

---
+++ LoginScreen.py
@@ -0,0 +1,21 @@
+from LoginOpEnum import LoginOpEnum
+
+class LoginScreen:
+
+    def __init__(self):
+        pass
+
+    def display_login_form(self):
+        pass
+
+    def get_decision(self) -> LoginOpEnum:
+        pass
+
+    def get_login_name_input(self) -> str:
+        pass
+
+    def get_password_input(self) -> str:
+        pass
+
+    def failure(self):
+        pass
