180c6401cdf44aafc64ae9ce567a5f95b1c4e53d
XO-Appleton <1511735933@qq.com>
2022-07-06 14:28:21 -0400

    commit test

--- IssueEditingScreen.py
+++
@@ -1,6 +0,0 @@
-class IssueEditingScreen:
-    def display_form():
-        iid = input("Enter issue number of the issue that you wish to edit: ")
-        name = input("Enter the new issue title: ")
-        description = input("Eneter the new issue description: ")
-        return (iid, name, description)
